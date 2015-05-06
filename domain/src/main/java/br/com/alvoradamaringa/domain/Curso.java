d8548bfcc9b56d2b61add0737f3f34f3d53990ca
CassioKuriyama <cassiokuriyama@gmail.com>
2015-05-06 19:25:00 -0300

    Classe Curso

--- domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
@@ -1,6 +1,21 @@
 package br.com.alvoradamaringa.domain;
 
-public class Curso {
+ 
+ public class Curso {
      private long idCurso;
      private String descricao;   
-}
+     
+     public void setId(Long idcurso){
+          this.idcurso=idcurso;
+     }
+     public void setDescricao(String descricao){
+          this.descricao=descricao;
+     }
+     public Long getId(Long idcurso){
+          return this.idcurso;
+     }
+     public String getDescricao(String descricao){
+          return this.descricao;
+     }
+     
+ }
